8abb559058e65c7a95ea6fbf79c1cbcca551749f
peng49 <1219955253@qq.com>
2020-06-29 21:13:25 +0800

    用户密码加密

--- fly-frontend/src/main/java/fly/frontend/SpringMainApplication.java
+++ fly-frontend/src/main/java/fly/frontend/SpringMainApplication.java
@@ -3,11 +3,19 @@ package fly.frontend;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @MapperScan("fly.frontend.mapper")
 public class SpringMainApplication {
     public static void main(String[] args) {
         org.springframework.boot.SpringApplication.run(SpringMainApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getBCryptPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
