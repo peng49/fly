b06b82ad0da6d5da8bb0b755abd10391f879fb8f
peng49 <1219955253@qq.com>
2020-08-24 10:46:21 +0800

    添加UploadController.java

---
+++ fly-frontend/src/main/java/fly/frontend/controller/UploadController.java
@@ -0,0 +1,10 @@
+package fly.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/upload")
+public class UploadController {
+
+}
