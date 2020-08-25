74081001d854ca7e9fd11e07c5bf934aeec124c7
peng49 <1219955253@qq.com>
2020-08-25 14:19:27 +0800

    添加lombok依赖

--- fly-frontend/src/main/java/fly/frontend/entity/Post.java
+++ fly-frontend/src/main/java/fly/frontend/entity/Post.java
@@ -1,8 +1,11 @@
 package fly.frontend.entity;
 
+import lombok.Data;
+
 import java.sql.Date;
 import java.sql.Timestamp;
 
+@Data
 public class Post {
     private int id;
     private Column column;
@@ -19,143 +22,4 @@ public class Post {
     private Timestamp updateAt;
     private int essence;
     private int top;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Column getColumn() {
-        return column;
-    }
-
-    public void setColumn(Column column) {
-        this.column = column;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getOriginalContent() {
-        return originalContent;
-    }
-
-    public void setOriginalContent(String originalContent) {
-        this.originalContent = originalContent;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public int getViewCount() {
-        return viewCount;
-    }
-
-    public void setViewCount(int viewCount) {
-        this.viewCount = viewCount;
-    }
-
-    public int getReplyCount() {
-        return replyCount;
-    }
-
-    public void setReplyCount(int replyCount) {
-        this.replyCount = replyCount;
-    }
-
-    public double getHeat() {
-        return heat;
-    }
-
-    public void setHeat(double heat) {
-        this.heat = heat;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public Timestamp getPublishAt() {
-        return publishAt;
-    }
-
-    public void setPublishAt(Timestamp publishAt) {
-        this.publishAt = publishAt;
-    }
-
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Timestamp getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Timestamp updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public int getEssence() {
-        return essence;
-    }
-
-    public void setEssence(int essence) {
-        this.essence = essence;
-    }
-
-    public int getTop() {
-        return top;
-    }
-
-    public void setTop(int top) {
-        this.top = top;
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", column=" + column +
-                ", author=" + author +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", viewCount=" + viewCount +
-                ", replyCount=" + replyCount +
-                ", status=" + status +
-                ", publishAt=" + publishAt +
-                ", createdAt=" + createdAt +
-                ", updateAt=" + updateAt +
-                ", essence=" + essence +
-                ", top=" + top +
-                '}';
-    }
 }
--- fly-frontend/src/main/java/fly/frontend/entity/PostComment.java
+++ fly-frontend/src/main/java/fly/frontend/entity/PostComment.java
@@ -1,7 +1,11 @@
 package fly.frontend.entity;
 
+import lombok.Data;
+
 import java.sql.Date;
 
+
+@Data
 public class PostComment {
     private int id;
     private User user;
@@ -10,72 +14,4 @@ public class PostComment {
     private String content;
     private Date commentTime;
     private int agreeCount;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Post getPost() {
-        return post;
-    }
-
-    public void setPost(Post post) {
-        this.post = post;
-    }
-
-    public PostComment getParent() {
-        return parent;
-    }
-
-    public void setParent(PostComment parent) {
-        this.parent = parent;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getCommentTime() {
-        return commentTime;
-    }
-
-    public void setCommentTime(Date commentTime) {
-        this.commentTime = commentTime;
-    }
-
-    public int getAgreeCount() {
-        return agreeCount;
-    }
-
-    public void setAgreeCount(int agreeCount) {
-        this.agreeCount = agreeCount;
-    }
-
-    @Override
-    public String toString() {
-        return "PostComment{" +
-                "id=" + id +
-                ", user=" + user +
-                ", post=" + post +
-                ", content='" + content + '\'' +
-                ", commentTime=" + commentTime +
-                ", agreeCount=" + agreeCount +
-                '}';
-    }
 }
