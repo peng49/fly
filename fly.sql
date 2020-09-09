14a05df24a4ab783275b2abf4b3aebb79e9a3a79
peng49 <1219955253@qq.com>
2020-09-09 10:30:54 +0800

    修改sql建表字段默认值

--- fly.sql
+++ fly.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `columns`;
 CREATE TABLE `columns`  (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `sort` int(11) NULL DEFAULT NULL,
+  `sort` int(11) NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -46,9 +46,9 @@ CREATE TABLE `friend_links`  (
 DROP TABLE IF EXISTS `post_comments`;
 CREATE TABLE `post_comments`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NULL DEFAULT NULL,
-  `post_id` int(11) NULL DEFAULT NULL,
-  `parent_id` int(11) NULL DEFAULT NULL,
+  `user_id` int(11) NULL DEFAULT 0,
+  `post_id` int(11) NULL DEFAULT 0,
+  `parent_id` int(11) NULL DEFAULT 0,
   `agree_count` int(11) NULL DEFAULT 0,
   `level` int(11) NULL DEFAULT 1,
   `comment_time` datetime(0) NULL DEFAULT NULL,
@@ -62,9 +62,9 @@ DROP TABLE IF EXISTS `post_comment_agree`;
 CREATE TABLE `post_comment_agree`
 (
   `id`         int(11) NOT NULL AUTO_INCREMENT,
-  `user_id`    int(11)  DEFAULT NULL COMMENT '用户Id',
-  `post_id`    int(11)  DEFAULT NULL,
-  `comment_id` int(11)  DEFAULT NULL,
+  `user_id`    int(11)  DEFAULT 0 COMMENT '用户Id',
+  `post_id`    int(11)  DEFAULT 0,
+  `comment_id` int(11)  DEFAULT 0,
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB  DEFAULT CHARSET = utf8;
@@ -75,20 +75,20 @@ CREATE TABLE `post_comment_agree`
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `column_id` int(11) NULL DEFAULT NULL,
-  `author_id` int(11) NULL DEFAULT NULL COMMENT 'users 表Id',
+  `column_id` int(11) NULL DEFAULT 0,
+  `author_id` int(11) NULL DEFAULT 0 COMMENT 'users 表Id',
   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `original_content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `status` tinyint(1) NULL DEFAULT NULL,
-  `reply_count` int(11) NULL DEFAULT NULL,
-  `view_count` int(11) NULL DEFAULT NULL,
+  `status` tinyint(1) NULL DEFAULT 0,
+  `reply_count` int(11) NULL DEFAULT 0,
+  `view_count` int(11) NULL DEFAULT 0,
   `publish_at` datetime(0) NULL DEFAULT NULL,
-  `heat` double(12, 4) NULL DEFAULT NULL COMMENT '热度',
+  `heat` double(12, 4) NULL DEFAULT 0 COMMENT '热度',
   `created_at` datetime(0) NULL DEFAULT NULL,
   `update_at` datetime(0) NULL DEFAULT NULL,
-  `essence` tinyint(1) NULL DEFAULT NULL COMMENT '是否加精',
-  `top` tinyint(1) NULL DEFAULT NULL COMMENT '是否置顶',
+  `essence` tinyint(1) NULL DEFAULT 0 COMMENT '是否加精',
+  `top` tinyint(1) NULL DEFAULT 0 COMMENT '是否置顶',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -146,8 +146,8 @@ CREATE TABLE `message_queue` (
   `title` varchar(255) DEFAULT NULL COMMENT '标题',
   `content` varchar(255) DEFAULT NULL COMMENT '内容',
   `attachment` varchar(255) DEFAULT NULL COMMENT '附件',
-  `status` tinyint(255) DEFAULT NULL COMMENT '状态',
-  `fail_number` int(11) DEFAULT '0' COMMENT '失败次数',
+  `status` tinyint(255) DEFAULT 0 COMMENT '状态',
+  `fail_number` int(11) DEFAULT 0 COMMENT '失败次数',
   `fail_message` varchar(255) DEFAULT NULL COMMENT '失败原因',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
