bce1dd704f702b1e54c28ff92bbee939c764b647
peng49 <1219955253@qq.com>
2020-10-15 18:04:23 +0800

    添加后台相关表

---
+++ fly-admin/src/main/java/fly/admin/entity/model/AdminMenu.java
@@ -0,0 +1,4 @@
+package fly.admin.entity.model;
+
+public class AdminMenu {
+}
---
+++ fly-admin/src/main/java/fly/admin/entity/model/AdminUser.java
@@ -0,0 +1,34 @@
+package fly.admin.entity.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "admin_users")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
+public class AdminUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String avatar;
+
+    private String rememberToken;
+
+    private Timestamp createdAt;
+
+    private Timestamp updateAt;
+}
