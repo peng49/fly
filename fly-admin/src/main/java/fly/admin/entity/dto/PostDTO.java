647a7a96a1c97959e7b51df41b4b0bf4c011816b
peng49 <1219955253@qq.com>
2020-10-22 14:20:46 +0800

    优化查询结果

---
+++ fly-admin/src/main/java/fly/admin/entity/dto/PostDTO.java
@@ -0,0 +1,18 @@
+package fly.admin.entity.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+public class PostDTO {
+    private Integer id;
+    private String title;
+    private UserDTO author;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Timestamp publishAt;
+}
