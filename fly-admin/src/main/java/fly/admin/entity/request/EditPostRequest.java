b7a3bdd7f1181895517a3f63d20215dbe2946057
peng49 <1219955253@qq.com>
2020-10-23 15:50:41 +0800

    编辑文章

--- fly-admin/src/main/java/fly/admin/entity/request/EditPostRequest.java
+++ fly-admin/src/main/java/fly/admin/entity/request/EditPostRequest.java
@@ -1,11 +1,22 @@
 package fly.admin.entity.request;
 
+
 import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class EditPostRequest {
+    @NotNull
+    @Min(1)
     private int columnId;
+
+    @NotNull
+    @Min(1)
     private int authorId;
+
+    @NotNull
     private String title;
     private String originalContent;
     private String content;
