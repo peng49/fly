9f46220174c8bc6e59ca9b86397233b3ecfa4c95
peng49 <1219955253@qq.com>
2020-10-30 18:45:33 +0800

    数据库权限配置

--- fly-admin/src/main/java/fly/admin/config/WebSecurityConfiguration.java
+++ fly-admin/src/main/java/fly/admin/config/WebSecurityConfiguration.java
@@ -1,29 +1,48 @@
 package fly.admin.config;
 
+import fly.admin.entity.model.AdminUser;
+import fly.admin.repository.AdminUserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.annotation.Resource;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 
-@Configuration
 @Slf4j
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     static final String[] ORIGINS = new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"};
 
     @Resource
     private UserDetailsService userDetailsService;
+
+    @Resource
+    private AdminUserRepository adminUserRepository;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -36,23 +55,58 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.cors();//允许跨域访问
-
-        httpSecurity.formLogin()//允许表单登录
+        httpSecurity
+                //允许跨域访问
+                .cors()
+                .and()
+                //基于token，所以不需要session
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()//允许表单登录
                 .successHandler((request, response, authentication) -> {//设置登录成功之后的操作
+                    UserDetails details = (UserDetails) authentication.getDetails();
+
                     response.getWriter().print("{\"code\":\"success\",\"data\":{\"token\":\"admin-token\"}}");
                 }).failureHandler((request, response, exception) -> {
                     log.debug(exception.getMessage());
-                    response.getWriter().println("{\"code\":\"exception\",\"message\":\"login fail\""+exception.getMessage()+"}");
+                    response.getWriter().println("{\"code\":\"exception\",\"message\":\"login fail\"" + exception.getMessage() + "}");
                 }
         );
 
         //https://www.cnblogs.com/l1ng14/p/13530416.html
         httpSecurity.csrf().disable();//暂时禁用csrf
 
-//        httpSecurity.authorizeRequests()
-//                .antMatchers("/api/users/**")
-//                .hasAnyAuthority("administrator");
+        httpSecurity.addFilterBefore(new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                //请求是否带有token
+                String token = request.getHeader("X-Token");
+                log.info("header token:" + token);
+                //todo 验证token是否有效 -> 通过token获取用户信息 -> 如果有效保存用户的相关信息
+
+                AdminUser adminUser = adminUserRepository.findByUsername("admin");
+                fly.admin.service.UserDetails details = new fly.admin.service.UserDetails(adminUser);
+
+                //userDetailsService
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        details, null, details.getAuthorities()
+                );
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                //设置用户登录状态
+                log.info("authenticated user {}, setting security context", token);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+                filterChain.doFilter(request, response);
+            }
+        }, UsernamePasswordAuthenticationFilter.class);
     }
 
 //    @Override
@@ -65,6 +119,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
+
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
--- fly-admin/src/main/java/fly/admin/service/impl/UserDetailsServiceImpl.java
+++ fly-admin/src/main/java/fly/admin/service/impl/UserDetailsServiceImpl.java
@@ -1,23 +1,15 @@
 package fly.admin.service.impl;
 
-import fly.admin.entity.model.AdminRole;
 import fly.admin.entity.model.AdminUser;
-import fly.admin.entity.model.AdminUserRole;
 import fly.admin.repository.AdminRoleRepository;
 import fly.admin.repository.AdminUserRepository;
 import fly.admin.repository.AdminUserRoleRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import fly.admin.service.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -25,65 +17,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Resource
     private AdminUserRepository adminUserRepository;
 
-    @Resource
-    private AdminUserRoleRepository adminUserRoleRepository;
-
-    @Resource
-    private AdminRoleRepository adminRoleRepository;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         AdminUser user = adminUserRepository.findByUsername(username);
         if(user == null){
             throw new UsernameNotFoundException(" not fount user by "+username);
         }
-        return new UserDetails() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                List<AdminUserRole> roleList = adminUserRoleRepository.findByUserId(user.getId());
-
-                List<AdminRole> roles = adminRoleRepository.findAllById(
-                        roleList.stream()
-                                .map(AdminUserRole::getRoleId)
-                                .collect(Collectors.toList())
-                );
-
-                List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                roles.forEach(role -> {
-                    authorities.add(new SimpleGrantedAuthority(role.getSlug()));
-                });
-                return authorities;
-            }
-
-            @Override
-            public String getPassword() {
-                return user.getPassword();
-            }
-
-            @Override
-            public String getUsername() {
-                return user.getUsername();
-            }
-
-            @Override
-            public boolean isAccountNonExpired() {
-                return true;
-            }
-
-            @Override
-            public boolean isAccountNonLocked() {
-                return true;
-            }
-
-            @Override
-            public boolean isCredentialsNonExpired() {
-                return true;
-            }
-
-            @Override
-            public boolean isEnabled() {
-                return true;
-            }
-        };
+        return new UserDetails(user);
     }
 }
