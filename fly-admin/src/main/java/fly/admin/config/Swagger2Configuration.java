340f2e2284944f0f25df8ebf5becbf2d72bf9c35
peng49 <1219955253@qq.com>
2020-10-30 14:23:12 +0800

    完善功能

--- fly-admin/src/main/java/fly/admin/config/Swagger2Configuration.java
+++ fly-admin/src/main/java/fly/admin/config/Swagger2Configuration.java
@@ -1,4 +1,4 @@
-package fly.admin.configuration;
+package fly.admin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- fly-admin/src/main/java/fly/admin/repository/PostCommentRepository.java
+++ fly-admin/src/main/java/fly/admin/repository/PostCommentRepository.java
@@ -4,5 +4,5 @@ import fly.admin.entity.model.PostComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostCommentRepository extends JpaRepository<PostComment,Integer> {
-
+    int countByUserId(int userId);
 }
