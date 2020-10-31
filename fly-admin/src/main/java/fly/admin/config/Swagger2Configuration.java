e64f2eddcedac37ab2ee4917a6839bb2be5cd73c
peng49 <1219955253@qq.com>
2020-10-31 17:50:21 +0800

    优化

--- fly-admin/src/main/java/fly/admin/config/Swagger2Configuration.java
+++ fly-admin/src/main/java/fly/admin/config/Swagger2Configuration.java
@@ -3,12 +3,18 @@ package fly.admin.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -34,10 +40,23 @@ public class Swagger2Configuration {
     }
 
     @Bean
-    public Docket createThemeApi(){
+    public Docket createThemeApi() {
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(
+                new ParameterBuilder()
+                        .name("X-Token")
+                        .parameterType("Header")
+                        .required(true)
+                        .description("Token")
+                        .modelRef(new ModelRef("string"))
+                        .build()
+        );
+
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("主题管理")
                 .apiInfo(new ApiInfoBuilder().title("主题管理").build())
+                .globalOperationParameters(parameters) //全局参数设置
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("fly.admin"))
                 .paths(PathSelectors.ant("/api/**"))
