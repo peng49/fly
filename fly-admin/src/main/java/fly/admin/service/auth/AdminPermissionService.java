0beaffcd26b2dde39b46d079e5f93e9cd40bd4f5
peng49 <1219955253@qq.com>
2020-11-02 17:51:14 +0800

    filter异常处理

--- fly-admin/src/main/java/fly/admin/service/auth/AdminPermissionService.java
+++ fly-admin/src/main/java/fly/admin/service/auth/AdminPermissionService.java
@@ -5,6 +5,10 @@ import fly.admin.entity.model.AdminPermission;
 import java.util.List;
 
 public interface AdminPermissionService {
+    String METHOD_SPLIT = ",";
+
+    String PATH_SPLIT = "\n";
+
     AdminPermission add(AdminPermission permission);
 
     void delete(AdminPermission permission);
