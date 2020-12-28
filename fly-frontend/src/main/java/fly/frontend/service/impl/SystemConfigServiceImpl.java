fcdeb66def42ece4a3e9ba1ae3a56827bb41c5bf
peng49 <1219955253@qq.com>
2020-12-28 13:40:49 +0800

    json序列化long转为string

--- fly-frontend/src/main/java/fly/frontend/service/impl/SystemConfigServiceImpl.java
+++ fly-frontend/src/main/java/fly/frontend/service/impl/SystemConfigServiceImpl.java
@@ -4,9 +4,11 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import fly.frontend.dao.SystemConfigMapper;
 import fly.frontend.entity.model.SystemConfig;
 import fly.frontend.service.SystemConfigService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SystemConfigServiceImpl extends ServiceImpl<SystemConfigMapper, SystemConfig> implements SystemConfigService {
     @Override
     public String getValue(String attribute) {
