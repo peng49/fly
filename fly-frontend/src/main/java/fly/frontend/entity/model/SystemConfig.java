de63f5a8a8021a588bb1018e296f9566e68c66a8
peng49 <1219955253@qq.com>
2020-12-28 11:45:48 +0800

    添加system_config表配置数据

---
+++ fly-frontend/src/main/java/fly/frontend/entity/model/SystemConfig.java
@@ -0,0 +1,30 @@
+package fly.frontend.entity.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@TableName(value = "system_config")
+public class SystemConfig {
+    @TableId(type = IdType.AUTO)
+    private int id;
+
+    private String attribute;
+
+    private String value;
+
+    private String remark;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
--- fly-frontend/src/main/java/fly/frontend/service/impl/GithubOauthServiceImpl.java
+++ fly-frontend/src/main/java/fly/frontend/service/impl/GithubOauthServiceImpl.java
@@ -9,7 +9,6 @@ import fly.frontend.entity.vo.GithubUserInfo;
 import fly.frontend.service.OauthAccountService;
 import fly.frontend.service.OauthService;
 import fly.frontend.service.UserService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -29,15 +28,6 @@ import java.util.Objects;
 public class GithubOauthServiceImpl implements OauthService {
     public static final String PLATFORM = "github";
 
-    @Value("${oauth.github.client-id}")
-    private String clientId;
-
-    @Value("${oauth.github.client-secret}")
-    private String clientSecret;
-
-    @Value("${oauth.github.redirect-uri}")
-    private String redirectUri;
-
     @Resource
     private RestTemplate restTemplate;
 
@@ -53,8 +43,14 @@ public class GithubOauthServiceImpl implements OauthService {
     @Resource
     private HttpSession httpSession;
 
+    @Resource
+    private SystemConfigServiceImpl systemConfigService;
+
     @Override
     public String getRedirectUrl() throws UnsupportedEncodingException {
+        String clientId = systemConfigService.getValue("github_oauth_client_id");
+        String redirectUri = systemConfigService.getValue("github_oauth_redirect_uri");
+
         return String.format("https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s&scope=user", clientId, URLEncoder.encode(redirectUri, "UTF-8"));
     }
 
@@ -64,6 +60,10 @@ public class GithubOauthServiceImpl implements OauthService {
      */
     @Override
     public OauthAccount get(String code) {
+        String clientId = systemConfigService.getValue("github_oauth_client_id");
+        String redirectUri = systemConfigService.getValue("github_oauth_redirect_uri");
+        String clientSecret = systemConfigService.getValue("github_oauth_client_secret");
+
         MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
         parameters.add("code", code);
         parameters.add("client_id", clientId);
