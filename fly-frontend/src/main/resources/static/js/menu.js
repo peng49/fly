24e83a2b728ca0260f211ce72148eb1ccb98a281
peng49 <1219955253@qq.com>
2020-12-29 16:54:39 +0800

    优化自动生成目录

--- fly-frontend/src/main/resources/static/js/menu.js
+++ fly-frontend/src/main/resources/static/js/menu.js
@@ -5,24 +5,32 @@ let content = document.getElementsByClassName('detail-body')[0].innerHTML
 //通过正则获取html内容中的所有html标签
 let matches = content.match(/<h(\d) .*?>.*?<\/h\d>/g)
 //遍历标签生成一个目录数组
-let tree = []
+let allMenu = []
 let rootLevel = 1
 let parents = []
 for (i in matches) {
     let h = matches[i]
     let m = h.match(/<h(\d) /)
     let level = Number(m[1])
-    if (tree.length === 0) {
+    if (allMenu.length === 0) {
         rootLevel = level
     }
     let con = h.replace(/<\/?\w.*?>/g, '')
     //console.log(con)
     console.log(level)
     let id = Number(i) + 1
-    let target = h.match(/<h\d id="(.*?)"/)[1]
+    let target = h.match(/<h\d id="(.*?)"/)[1];
+
+    if ($('[id="' + target + '"]').length > 1) {
+        let newId = target + Math.random().toString(36).slice(-10);
+        $($('[id=' + target + ']').get(0)).attr('id', newId)
+        target = newId
+    }
+    console.log(target)
+    let offsetTop = $('#'+target).offset().top;
 
     if (rootLevel === level) {
-        tree.push({id: id, name: con, target: target, level: level, parentId: 0})
+        allMenu.push({id: id, name: con, target: target, offsetTop: offsetTop, level: level, parentId: 0})
 
         parents = [{id: 0, level: 1}, {id: id, level: level}]
     } else {
@@ -30,14 +38,21 @@ for (i in matches) {
             parents.pop()
         }
 
-        tree.push({id: id, name: con, target: target, level: level, parentId: parents[parents.length - 1].id})
+        allMenu.push({
+            id: id,
+            name: con,
+            target: target,
+            offsetTop: offsetTop,
+            level: level,
+            parentId: parents[parents.length - 1].id
+        });
 
         if (level - parents[parents.length - 1].level > 1) {
             parents.push({id: id, level: level})
         }
     }
 }
-console.log(tree)
+console.log(allMenu)
 
 //渲染数组生成标签
 function getChildren(items, parent) {
@@ -56,10 +71,10 @@ function getChildren(items, parent) {
 }
 
 let result = []
-for (i in tree) {
-    let item = tree[i]
+for (i in allMenu) {
+    let item = allMenu[i]
     if (item.parentId == 0) {
-        item.children = getChildren(tree, item)
+        item.children = getChildren(allMenu, item)
         result.push(item)
     }
 }
@@ -83,27 +98,47 @@ let ul = "<ul id='post-menus'>" + renderTree(result, 1) + "</ul>";
 
 $("#slider").append('<div id="menu-box" class="fly-panel slider-menu"><div class="fly-panel-title">目录</div><div class="fly-panel-main">' + ul + '</div></div>');
 
+//点击目录滑动到指定div
 $("body").on("click", "#post-menus a", function (e) {
     let id = $(e.target).attr("data-target")
     $(window).scrollTop(document.getElementById(id).offsetTop)
-})
+});
 
-let menuDom = $('#menu-box')
-let offsetTop = $('#slider').offset().top
-let width = $('#slider').width()
-let menuOffsetTop = $("#menu-box").offset().top
-let scrollFlag = true
+let menuDom = $('#menu-box');
+let offsetTop = $('#slider').offset().top;
+let width = $('#slider').width();
+let menuOffsetTop = $("#menu-box").offset().top;
+let scrollFlag = true;
 let clientHeight = $(window).height();
+
+function getActiveMenu()
+{
+    let scrollTop = $(window).scrollTop()
+    for(let i in allMenu){
+        let menu = allMenu[i];
+        if(menu.offsetTop > scrollTop){
+            return menu
+        }
+    }
+    return null;
+}
+
 $(window).scroll(function () {
-    console.log(scrollFlag)
+    console.log(scrollFlag);
     if ($(window).scrollTop() > menuOffsetTop) {
         if (scrollFlag) {
-            menuDom.css({position: "fixed", top: offsetTop, width: width})
+            menuDom.css({position: "fixed", top: offsetTop, width: width});
             menuDom.find('.fly-panel-main').css({overflowY:'scroll',maxHeight:clientHeight+'px'});
             scrollFlag = false
         }
     } else {
-        menuDom.attr("style", "")
+        menuDom.attr("style", "");
         scrollFlag = true
     }
-})
+
+    let activeMenu = getActiveMenu();
+    if(activeMenu !== null){
+        $('#menu-box a').removeClass('active');
+        $('#menu-box a[data-target="'+activeMenu.target+'"]').addClass('active')
+    }
+});
