d6b21bb73d747a998875c65d7d04e69eff549e50
peng49 <1219955253@qq.com>
2021-01-02 16:10:51 +0800

    优化查询

--- fly-frontend/src/main/java/fly/frontend/dao/PostMapper.java
+++ fly-frontend/src/main/java/fly/frontend/dao/PostMapper.java
@@ -2,15 +2,16 @@ package fly.frontend.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import fly.frontend.entity.model.Post;
-import fly.frontend.entity.model.PostComment;
-import fly.frontend.entity.from.PostFilterCondition;
-import org.apache.ibatis.annotations.*;
-
-import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface PostMapper extends BaseMapper<Post> {
 
+    @Select("select p.* from posts as p inner join user_posts as up on up.post_id = p.id where up.user_id = #{userId} order by up.created_at desc")
+    IPage<Post> findForUserCollection(Long userId, IPage page);
+
+    @Select("select count(0) from posts as p inner join user_posts as up on up.post_id = p.id where up.user_id = #{userId}")
+    int findUserCollectionTotal(Long userId);
 }
