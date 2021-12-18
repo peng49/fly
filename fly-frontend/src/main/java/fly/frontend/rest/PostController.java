b27c6ed92270c08d6dbffe9c70a28c30c65a999c
peng49 <1219955253@qq.com>
2021-12-18 17:02:31 +0800

    add rest

---
+++ fly-frontend/src/main/java/fly/frontend/rest/PostController.java
@@ -0,0 +1,17 @@
+package fly.frontend.rest;
+
+import fly.frontend.entity.vo.ResultVO;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PostController {
+    /**
+     * 保存/编辑文章
+     *
+     * @return object
+     */
+    public ResultVO edit() {
+        return null;
+    }
+}
