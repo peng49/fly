053c658b6514827d711676a699a29a8951331d13
peng49 <1219955253@qq.com>
2023-07-14 15:39:55 +0800

    编辑entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,22 @@
-#!/usr/bin/bash
+#!/bin/bash
 
+FLY_PATH=/var/www/
+
+params=$*
+
+project="${1}"
+if [ "$project" = "" ]; then
+    project="web"
+    params="web"
+fi
+
+params=${params/${project}/""}
+
+case "$project" in
+  "web")
+      java -jar ${FLY_PATH}fly-web/application.jar -f ${FLY_PATH}fly-web/application.yml ${params}
+    ;;
+  "admin")
+      java -jar ${FLY_PATH}fly-admin/application.jar -f ${FLY_PATH}fly-admin/application.yml ${params}
+    ;;
+esac
