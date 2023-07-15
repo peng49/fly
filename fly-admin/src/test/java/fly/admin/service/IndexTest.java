d486fafe282de03f06e5cfb9c5d7415901dba863
peng49 <1219955253@qq.com>
2023-07-16 01:13:05 +0800

    读取jar中的index.html失败处理

--- fly-admin/src/test/java/fly/admin/service/IndexTest.java
+++ fly-admin/src/test/java/fly/admin/service/IndexTest.java
@@ -1,12 +1,17 @@
 package fly.admin.service;
 
 import junit.framework.TestCase;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import sun.misc.IOUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class IndexTest extends TestCase {
     public void testReadIndex() throws IOException {
@@ -16,15 +21,33 @@ public class IndexTest extends TestCase {
         String currentDir = System.getProperty("user.dir");
         System.out.println("Current dir using System:" + currentDir);
 
-        URL url = getClass().getResource("/vue/dist/index.html");
-        System.out.println(url.getPath().replaceAll("^/([A-Z]:)","$1"));
+        URL url = getClass().getResource("/application-prod.yml");
+        System.out.println(url.getFile());
+
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application-prod.yml");
+        System.out.println(inputStream);
+
+        assert inputStream != null;
+        Scanner scanner = new Scanner(inputStream).useDelimiter("\n");
+        StringBuilder content = new StringBuilder();
+        do{
+            content.append("\n").append(scanner.next());
+        }while (scanner.hasNext());
+        System.out.println(content);
+
+
+
+
+        Resource resource = new ClassPathResource("/application-prod.yml");
+
 
 
 
 //        String content = readFile("test.txt", StandardCharsets.UTF_8);
 //        Path path = Paths.get();
 //        BufferedReader reader = new BufferedReader(new FileReader(url.getPath()));
-        byte[] encoded = Files.readAllBytes(Paths.get(url.getPath().replaceAll("^/([A-Z]:)","$1")));
-        System.out.println(new String(encoded, StandardCharsets.UTF_8));
+//        byte[] encoded = Files.readAllBytes(Paths.get(url.getPath().replaceAll("^/([A-Z]:)","$1")));
+//        System.out.println(new String(encoded, StandardCharsets.UTF_8));
     }
 }
