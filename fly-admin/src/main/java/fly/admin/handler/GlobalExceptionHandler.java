cd9db71cb7ae64a39e061b76189d5549f3a83b7e
peng49 <1219955253@qq.com>
2023-07-24 17:06:33 +0800

    ExceptionHandler加载顺序问题处理;token失效后转跳重新登录

--- fly-admin/src/main/java/fly/admin/handler/GlobalExceptionHandler.java
+++ fly-admin/src/main/java/fly/admin/handler/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package fly.admin.handler;
 
 import fly.admin.entity.vo.ResultVO;
 import fly.admin.exception.InvalidTokenException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,8 +18,9 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(InvalidTokenException.class)
+    @Order(Ordered.HIGHEST_PRECEDENCE) //值越小越优先加载
     @ResponseBody
-    public final ResultVO handleAccessExceptions(Exception ex,WebRequest request){
+    public final ResultVO handleAccessExceptions(Exception ex, WebRequest request) {
         return ResultVO.builder().code("invalid.token").message(ex.getMessage()).build();
     }
 }
